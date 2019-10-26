6f85f79f9767744b176821f4dbca3c68ce5fd971
Samma <46258134+Samunter@users.noreply.github.com>
2019-10-26 00:57:05 -0700

    oh wow I forgot to commit for a long time

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,51 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      { phrase: 'Live long and prosper' },
+      { phrase: 'What goes around comes around' },
+      { phrase: 'Bibbidy Bobbidy Boo' },
+      { phrase: 'Count me in' },
+      { phrase: 'A bird in the hand is worth two in the bush' }
+    ];
+    this.activePhrase = null;
+  }
+
+  getRandomPhrase() {
+    let i = Math.floor(Math.random() * this.phrases.length);
+    // console.log('phrase index: ', i);
+    return new Phrase(this.phrases[i].phrase);
+  }
+
+  startGame() {
+    document.getElementById('overlay').style.display = 'none';
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+  }
+
+  handleInteraction() {}
+}
+
+// const game = new Game();
+// game.phrases.forEach((phrase, index) => {
+//   console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+// });
+
+// const logPhrase = phrase => {
+//   console.log(`Phrase - phrase: `, phrase.phrase);
+// };
+
+// const game = new Game();
+
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+
+// const game = new Game();
+// game.getRandomPhrase().addPhraseToDisplay();
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,49 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    const parentDIV = document.getElementById('phrase');
+    // console.log('parentDIV: ', parentDIV);
+
+    for (let i = 0; i < this.phrase.length; i++) {
+      const character = this.phrase.charAt(i);
+      // console.log('character: ', character);
+      const charLI = document.createElement('li');
+      // console.log('charLI: ', charLI);
+
+      if (/ /.test(character)) {
+        charLI.setAttribute('class', 'space');
+        charLI.innerHTML = ' ';
+      } else {
+        charLI.setAttribute('class', `hide letter ${character}`);
+        charLI.innerHTML = character;
+      }
+      parentDIV.appendChild(charLI);
+    }
+  }
+
+  checkLetter(letter) {
+    return !!this.phrase.match(letter);
+  }
+
+  showMatchedLetter(letter) {
+    const charLIs = document.getElementsByClassName('letter');
+    // console.log("character LIs: ", charLIs);
+    for (let i = 0; i < charLIs.length; i++) {
+      const charLI = charLIs[i];
+      console.log('character LI inner html:', charLI.innerHTML);
+      if (letter === charLI.innerHTML) {
+        charLI.setAttribute('class', `show letter ${letter}`);
+      }
+    }
+  }
+}
+
+/* const examplePhrase = new Phrase('What up');
+console.log('examplePhrase.phrase: ', examplePhrase.phrase); */
